9f36b17ebdc4c30b1810cd46de916e99f4891e80
vmdev10 <mouraanav@gmail.com>
2021-09-21 21:56:29 -0300

    Crendentials changed

--- src/config/facebook_oAuth.js
+++ src/config/facebook_oAuth.js
@@ -1,18 +1,22 @@
 export const initFbsdk = () => {
   return new Promise(resolve => {
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = (function() {
       FB.init({
-        appId: '2507113472858546',
+        appId: "277484573985924",
         cookie: true, // enable cookies to allow the server to access the session
         xfbml: true, // parse social plugins on this page
-        version: 'v2.8' // use graph api version 2.8
-      })
-    }(function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) return
-      js = d.createElement(s); js.id = id
-      js.src = '//connect.facebook.net/en_US/all.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    }(document, 'script', 'facebook-jssdk'))
-  })
-}
+        version: "v2.8" // use graph api version 2.8
+      });
+    })(
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk")
+    );
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/router'
-import store from './store/store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import store from "./store/store";
 
-import '@/assets/css/style.css'
-import './registerServiceWorker'
+import "@/assets/css/style.css";
+import "./registerServiceWorker";
 
-import GoogleAuth from '@/config/google_oAuth.js'
+import GoogleAuth from "@/config/google_oAuth.js";
 const gauthOption = {
-  clientId: '707231563844-e5cpkqrlt62gncmj6b84of5sml9lp8g9.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account'
-}
-Vue.use(GoogleAuth, gauthOption)
-Vue.config.productionTip = false
+  clientId:
+    "1012329225066-5eask441gcok9mrspathp897b1m2rne0.apps.googleusercontent.com",
+  scope: "profile email",
+  prompt: "select_account"
+};
+Vue.use(GoogleAuth, gauthOption);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
